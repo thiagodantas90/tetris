3878d89d19968d9a15ddaaa3df49b0853da2061a
thiagodantas90 <thiagobd1990@gmail.com>
2019-09-12 19:09:59 -0300

    Iniciando projeto

---
+++ app/src/main/java/br/ufrn/eaj/tads/gametetris/telaInicial.kt
@@ -0,0 +1,14 @@
+package br.ufrn.eaj.tads.gametetris
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class telaInicial : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_inicial)
+    }
+
+}
